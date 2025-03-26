8ae7044d9622d8dd9067be98e686bc5a9bff8406
hsuyunyun <5b1g0011@stust.edu.tw>
2025-03-26 13:31:35 +0800

    123

---
+++ waterpuzzie.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    const gameContainer = document.getElementById('game-container');
+    const playButton = document.getElementById('play-button');
+    const levelSelect = document.getElementById('level-select');
+
+    const tubes = [];
+    let levelCount = 1;
+
+    levelSelect.addEventListener('change',(event)=>{
+        const selectedLevel = parseInt(event.target.value,10)
+    })
+});
